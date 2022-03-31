65074dbf513263c3b6c9e0ccc57c9d7a5905bcc1
marcio <marcio.vjr@gmail.com>
2022-03-31 11:03:05 -0300

    JPS Treinamento - Projeto C#

---
+++ AulasCSharp/Aula1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System;
+
+namespace Aula1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Estou chegando pra ganhar 15k");
+        }
+    }
+}
